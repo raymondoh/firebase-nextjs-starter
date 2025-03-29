a25c28bd75d465843f63e0c52cefa59f38235e62
Raymondo <raymondmhylton@gmail.com>
2025-03-29 04:05:36 +0000

    Switch to next.config.js to disable ESLint on Vercel

---
+++ next.config.js
@@ -0,0 +1,70 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  reactStrictMode: true,
+  webpack: config => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      net: false,
+      tls: false,
+      fs: false,
+      http2: false,
+      http: false,
+      https: false,
+      zlib: false,
+      child_process: false
+    };
+    return config;
+  },
+  serverExternalPackages: ["firebase-admin"],
+  images: {
+    domains: [
+      "storage.googleapis.com",
+      "lh3.googleusercontent.com",
+      "lh4.googleusercontent.com",
+      "lh5.googleusercontent.com",
+      "lh6.googleusercontent.com"
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/**"
+      }
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block"
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN"
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff"
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin"
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:;"
+          }
+        ]
+      }
+    ];
+  }
+};
+
+module.exports = nextConfig;
